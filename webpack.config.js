1ecd632be3ffaeb4ac10d2fedda251b5fc624b10
PovilasU <povilas.urbonas85@gmail.com>
2025-05-27 10:59:44 +0300

    updated minimisation of dist files

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Add this line
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "main.min.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Optional: cleans dist folder before build
   },
   module: {
     rules: [
@@ -17,15 +19,23 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "styles.min.css",
+    new MiniCssExtractPlugin({ filename: "styles.min.css" }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/about.html",
+      filename: "about.html",
+      minify: true,
     }),
   ],
   optimization: {
     minimize: true,
     minimizer: [
       `...`, // keep existing minimizers (like Terser for JS)
-      new CssMinimizerPlugin(), // add this for CSS
+      new CssMinimizerPlugin(),
     ],
   },
   mode: "production",
